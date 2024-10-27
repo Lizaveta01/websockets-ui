7ff19d8d8512ec563a8395f099232d43265d21e2
Lizaveta01 <goldglaider@gmail.com>
2024-10-27 22:34:30 +0300

    feat: config ts

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,8 +1,11 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import { WebSocketServer } from 'ws';
 
-export const httpServer = http.createServer(function (req, res) {
+const wss = new WebSocketServer({ noServer: true });
+
+export const server = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
     fs.readFile(file_path, function (err, data) {
@@ -13,5 +16,20 @@ export const httpServer = http.createServer(function (req, res) {
         }
         res.writeHead(200);
         res.end(data);
+    });
+});
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('Message:', message);
+    });
+
+    ws.send(JSON.stringify({ type: 'info', data: 'WebSocket подключен' }));
+});
+
+
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
     });
 });
